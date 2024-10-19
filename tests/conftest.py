1b657ee194c10c27c488e37845ae42b1167d6238
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2024-10-19 21:27:04 +0200

    Setup for snapshot-testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,11 @@
 """Tests for Habiticalib."""
 
 from collections.abc import Generator
+from functools import lru_cache
+import pathlib
 
 from aioresponses import aioresponses
+import orjson
 import pytest
 
 
@@ -11,3 +14,14 @@ def aioclient_mock() -> Generator[aioresponses]:
     """Mock Aiohttp client requests."""
     with aioresponses() as m:
         yield m
+
+
+@lru_cache
+def load_fixture(filename: str) -> str:
+    """Load a fixture."""
+
+    return orjson.loads(
+        pathlib.Path(__file__)
+        .parent.joinpath("fixtures", filename)
+        .read_text(encoding="utf-8")
+    )
---
+++ tests/test_user.py
@@ -0,0 +1,20 @@
+"""Tests for user methods of Habiticalib."""
+
+from aiohttp import ClientSession
+from aioresponses import aioresponses
+from syrupy.assertion import SnapshotAssertion
+
+from habiticalib import Habitica
+
+from .conftest import load_fixture
+
+
+async def test_user(mock_aiohttp: aioresponses, snapshot: SnapshotAssertion) -> None:
+    """Test default user agent is set."""
+    mock_aiohttp.get(
+        "https://habitica.com/api/v3/user", payload=load_fixture("user.json")
+    )
+    async with ClientSession() as session:
+        habitica = Habitica(session, "test", "test")
+        response = await habitica.get_user()
+        assert response == snapshot
