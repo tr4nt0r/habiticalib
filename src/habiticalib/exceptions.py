93959b7f08b3b48824f0d7395af2aed6f0c5f6c6
Manu <4445816+tr4nt0r@users.noreply.github.com>
2024-10-26 02:09:16 +0200

    Add `TooManyRequestsError`, pass ratelimits to exception (#11)
    Add `TooManyRequestsError`, pass ratelimit in exception

--- src/habiticalib/exceptions.py
+++ src/habiticalib/exceptions.py
@@ -2,15 +2,25 @@
 
 from typing import Self
 
+from multidict import CIMultiDictProxy
+
 from habiticalib.types import HabiticaErrorResponse
 
 
 class HabiticaException(Exception):  # noqa: N818
     """Base class for Habitica errors."""
 
-    def __init__(self: Self, error: HabiticaErrorResponse) -> None:
+    def __init__(
+        self: Self,
+        error: HabiticaErrorResponse,
+        headers: CIMultiDictProxy,
+    ) -> None:
         """Initialize the Exception."""
         self.error = error
+        self.rate_limit = headers.get("x-ratelimit-limit")
+        self.rate_limit_remaining = headers.get("x-ratelimit-remaining")
+        self.rate_limit_reset = headers.get("x-ratelimit-reset")
+        self.retry_after = headers.get("retry-after")
 
         super().__init__(error.message)
 
@@ -25,3 +35,7 @@ class NotFoundError(HabiticaException):
 
 class BadRequestError(HabiticaException):
     """BadRequest error."""
+
+
+class TooManyRequestsError(HabiticaException):
+    """TooManyRequests error."""
