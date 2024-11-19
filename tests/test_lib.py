dfaf83bfcfe5961d31929c25e07cb92cb0eb1553
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2024-11-20 00:08:47 +0100

    Add test for login method

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -9,6 +9,18 @@ from habiticalib import Habitica
 from .conftest import load_fixture
 
 
+async def test_login(mock_aiohttp: aioresponses, snapshot: SnapshotAssertion) -> None:
+    """Test login."""
+    mock_aiohttp.post(
+        "https://habitica.com/api/v3/user/auth/local/login",
+        body=load_fixture("login.json"),
+    )
+    async with ClientSession() as session:
+        habitica = Habitica(session, "test", "test")
+        response = await habitica.login("test-username", "test-password")
+        assert response == snapshot
+
+
 async def test_user(mock_aiohttp: aioresponses, snapshot: SnapshotAssertion) -> None:
     """Test default user agent is set."""
     mock_aiohttp.get("https://habitica.com/api/v3/user", body=load_fixture("user.json"))
