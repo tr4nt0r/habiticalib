4c9416f8497b18a298c3a4c5d524abee6598cc0a
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2025-02-04 17:28:27 +0100

    Add pytest markers for slow and linux-only tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,6 +24,29 @@ DEFAULT_HEADERS = {
 }
 
 
+def pytest_addoption(parser: pytest.Parser) -> None:
+    """Commandline options."""
+
+    parser.addoption(
+        "--runslow", action="store_true", default=False, help="run slow tests"
+    )
+
+
+def pytest_collection_modifyitems(
+    config: pytest.Config,
+    items: list[pytest.Item],
+) -> None:
+    """Check for slow items."""
+    if config.getoption("--runslow"):
+        # --runslow given in cli: do not skip slow tests
+        return
+
+    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
+    for item in items:
+        if "slow" in item.keywords:
+            item.add_marker(skip_slow)
+
+
 @pytest.fixture
 def api_url() -> URL:
     """Return base API URL."""
