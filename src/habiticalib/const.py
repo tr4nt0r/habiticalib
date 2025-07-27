b4ff946b29b5aafaa07f1288c27e4589e2748895
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2025-07-27 17:48:21 +0200

    Bump version 0.4.0 → 0.4.1

--- src/habiticalib/const.py
+++ src/habiticalib/const.py
@@ -1,6 +1,6 @@
 """Constants for Habiticalib."""
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 DEFAULT_URL = "https://habitica.com/"
 ASSETS_URL = "https://habitica-assets.s3.amazonaws.com/mobileApp/images/"
